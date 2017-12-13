adf691938af4a4de8d395e511df0f8b29bb9f2d7
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-12-13 19:59:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  const stringyTemplate = $('#page-template').html()
+  const pageTemplate = _.template(stringyTemaplate)
+  const pTemplate = $('post-template').html()
+  const postTemplate = _.template(pTemplate)
+  const commTemplate = $('#comments-template').html()
+  const commnetsTemplate = _.template(commTemplate)
+  const comTemplate = $('comment-template').html()
+  const commentTemplate = _.template(comTemplate)
+})
+
+function createPost(){
+  
+}
+function postComment(){
+  
+}
