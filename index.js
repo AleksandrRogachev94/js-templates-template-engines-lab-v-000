450ce8606839f2e62acadbc2758d6498cf072590
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-12-13 20:09:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
   let stringyTemplate = $('#page-template').html()
   const pageTemplate = _.template(stringyTemaplate)
-  
+
   stringyTemplate = $('post-template').html()
   const postTemplate = _.template(stringyTemplate)
-  
+
   stringyTemplate = $('#comments-template').html()
   const commnetsTemplate = _.template(stringyTemplate)
-  
+
   stringyTemplate = $('#comment-template').html()
   const commentTemplate = _.template(stringyTemplate)
 })
