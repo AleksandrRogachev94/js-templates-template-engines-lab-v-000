d23afb970c1bb08e5210445d768f5b4b997a0b7f
Aleksandr Rogachev <aleksandr.rogachev1994@gmail.com>
2017-12-13 20:42:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   let stringyTemplate = $('#page-template').html()
   const pageTemplate = _.template(stringyTemplate)
 
-  stringyTemplate = $('post-template').html()
+  stringyTemplate = $('#post-template').html()
   const postTemplate = _.template(stringyTemplate)
 
   stringyTemplate = $('#comments-template').html()
@@ -21,8 +21,7 @@ $(document).ready(() => {
     const myTitle = document.getElementById("postTitle").value;
     const myAuthor = document.getElementById("postAuthor").value;
     const myBody = document.getElementById("postBody").value;
-    const html = postTemplate({ title: myTitle, author: myAuthor, body: myBody })
-    // const html = pageTemplate({ title: myTitle, author: myAuthor, body: myBody, postTemplate: postTemplate })
+    const html = pageTemplate({ title: myTitle, author: myAuthor, body: myBody, postTemplate: postTemplate })
     pages.append(html)
   }
   function postComment(){
